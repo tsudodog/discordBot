9096b49fed45656f1511ab7dca930e150d1f7ecd
tsudo <malamute29@gmail.com>
2021-02-23 15:34:21 -0600

    changes

--- webserver/manage.py
+++ webserver/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import subprocess
 
 
 def main():
@@ -15,6 +16,12 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+
+    # completed = subprocess.run('dir', shell=True)
+
+    # completed = subprocess.run('python ./../bot/main.py', shell=True)
+    # print('returncode:', completed.returncode)
     execute_from_command_line(sys.argv)
 
 
--- webserver/webserver/settings.py
+++ webserver/webserver/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'djqd97=#=skrgyajbx4xifk_4+w5x#%*(_d!-$8hx#a*ea1&z$'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'djqd97=#=skrgyajbx4xifk_4+w5x#%*(_d!-$8hx#a*ea1&z$'
+# SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
